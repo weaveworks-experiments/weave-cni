c85b8ca5a8d63cc05b8d134b80b20f0abde9cbac
Bryan Boreham <bjboreham@gmail.com>
2016-03-07 15:44:13 +0000

    Use Weave Net API

--- ipam/weave/main.go
+++ ipam/weave/main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net"
-	"net/http"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/appc/cni/pkg/skel"
 	"github.com/appc/cni/pkg/types"
+	"github.com/weaveworks/weave/api"
 )
 
 func main() {
 	skel.PluginMain(cmdAdd, cmdDel)
 }
+
+var (
+	log      = logrus.New()
+	weaveapi = api.NewClient("localhost:6784", log)
+)
 
 func cmdAdd(args *skel.CmdArgs) error {
 	// extract the things we care about
@@ -23,23 +28,19 @@ func cmdAdd(args *skel.CmdArgs) error {
 	}
 
 	containerID := args.ContainerID
-	var url string
+	var ipnet *net.IPNet
 
 	if conf.Subnet == "" {
-		url = fmt.Sprintf("/ip/%s", containerID)
+		ipnet, err = weaveapi.AllocateIP(containerID)
 	} else {
-		_, err = types.ParseCIDR(conf.Subnet)
+		var subnet *net.IPNet
+		subnet, err = types.ParseCIDR(conf.Subnet)
 		if err != nil {
 			return fmt.Errorf("subnet given in config, but not parseable: %s", err)
 		}
-		url = fmt.Sprintf("/ip/%s/%s", containerID, conf.Subnet)
+		ipnet, err = weaveapi.AllocateIPInSubnet(containerID, subnet)
 	}
 
-	var ip string
-	if ip, err = httpPost(url); err != nil {
-		return err
-	}
-	ipnet, err := types.ParseCIDR(ip)
 	if err != nil {
 		return err
 	}
@@ -54,8 +55,7 @@ func cmdAdd(args *skel.CmdArgs) error {
 
 func cmdDel(args *skel.CmdArgs) error {
 	containerID := args.ContainerID
-	url := fmt.Sprintf("/ip/%s", containerID)
-	if err := httpDelete(url); err != nil {
+	if err := weaveapi.ReleaseIP(containerID); err != nil {
 		return err
 	}
 	result := types.Result{}
@@ -74,48 +74,4 @@ type netConf struct {
 func loadIPAMConf(stdinData []byte) (*ipamConf, error) {
 	var conf netConf
 	return conf.IPAM, json.Unmarshal(stdinData, &conf)
-}
-
-func httpPost(path string) (string, error) {
-	resp, err := http.Post("http://localhost:6784"+path, "", nil)
-	if err != nil {
-		return "", err
-	}
-	if !okResponse(resp) {
-		return "", fmt.Errorf("non-OK status code %d", resp.StatusCode)
-	}
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(bytes), nil
-}
-
-func httpDelete(path string) error {
-	req, err := http.NewRequest("DELETE", "http://localhost:6784"+path, nil)
-	if err != nil {
-		return err
-	}
-	client := http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	if !okResponse(resp) {
-		return fmt.Errorf("non-OK status code %d", resp.StatusCode)
-	}
-	return nil
-}
-
-func okResponse(response *http.Response) bool {
-	switch response.StatusCode {
-	case http.StatusOK:
-		return true
-	case http.StatusCreated:
-		return true
-	case http.StatusAccepted:
-		return true
-	default:
-		return false
-	}
 }
